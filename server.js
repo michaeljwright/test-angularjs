d88ffee337319aa8dffbbf45db81632b6919bb57
Stepan Suvorov <stevermeister@gmail.com>
2014-07-08 19:18:59 +0200

    server for testing

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * Just for testing
+ */
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/feed', function(req, res) {
+  res.sendfile('feed_test.json', { root: __dirname });
+});
+
+app.all('/*', function(req, res) {
+  res.sendfile('index.html', { root: __dirname });
+});
+
+app.listen(3000);
